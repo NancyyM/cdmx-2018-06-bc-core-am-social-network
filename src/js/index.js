c2922ab38ec85c20df51d711e3ee4d282fd93b34
unknown <nan.si_1305@hotmail.com>
2018-07-17 12:23:27 -0500

    subiendo proyecto a github

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAw5cZOgGoVoog9prX44MlN1415wt-sYgs",
+    authDomain: "socialnetwork-lab.firebaseapp.com",
+    databaseURL: "https://socialnetwork-lab.firebaseio.com",
+    projectId: "socialnetwork-lab",
+    storageBucket: "",
+    messagingSenderId: "535657697568"
+  };
+  firebase.initializeApp(config);
+</script>
