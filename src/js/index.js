4aecd395d4691c0ec96147805e5b258f6d57af17
Nancy Mejía Gómez <39561729+NancyyM@users.noreply.github.com>
2018-07-18 13:13:26 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCihx2_wwaHazrySwOO0LLmdBCWtfZgoek",
-    authDomain: "login-35451.firebaseapp.com",
-    databaseURL: "https://login-35451.firebaseio.com",
-    projectId: "login-35451",
-    storageBucket: "login-35451.appspot.com",
-    messagingSenderId: "276377229885"
-  };
-  firebase.initializeApp(config);
 
-  const usuario = document.getElementById('user');
-  const clave = document.getElementById('password');
-  const btn = document.getElementById('cuenta');
-  
-  // console.log(usuario);
-  
+var config = {
+  apiKey: "AIzaSyCihx2_wwaHazrySwOO0LLmdBCWtfZgoek",
+  authDomain: "login-35451.firebaseapp.com",
+  databaseURL: "https://login-35451.firebaseio.com",
+  projectId: "login-35451",
+  storageBucket: "login-35451.appspot.com",
+  messagingSenderId: "276377229885"
+};
+firebase.initializeApp(config);
+
+const usuario = document.getElementById('user');
+const clave = document.getElementById('password');
+const btn = document.getElementById('cuenta');
+
+// console.log(usuario);
+
 btn.addEventListener('click', e =>{
 const email = usuario.value;
 // console.log(email);
@@ -24,5 +25,6 @@ const auth = firebase.auth();
 
 const promise = auth.createUserWithEmailAndPassword(email, cl);
 promise.catch(e => console.log(e.message));
+
 
 });
