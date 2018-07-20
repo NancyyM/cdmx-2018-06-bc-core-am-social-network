ba6cd8489ec2b481119ea7d2dc00fff63430bb52
Josce Jaimes <nanayhachi94@gmail>
2018-07-19 22:23:42 -0500

    Agregandovalidaciones a correo electrónico

--- src/js/index.js
+++ src/js/index.js
@@ -11,156 +11,55 @@
   firebase.initializeApp(config);
 
 
-// creando usuario
 
 const usuario = document.getElementById('user1');
 const clave = document.getElementById('password1');
 const btn = document.getElementById('login');
 const btnUser = document.getElementById('checkIn');
+const mailCorrect = document.getElementById('emailCorrect')
+validateEmail = (user1)=>{
+  var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(user1) ? true : false;
+  
+};
 
 
-// Creacion de ususario.
-btn.addEventListener('click', e =>{
+// REGISTRARSE.
+btnUser.addEventListener('click', e =>{
 const mail = usuario.value;
-console.log(mail);
 const cl = clave.value;
 const auth = firebase.auth();
 const promise = auth.createUserWithEmailAndPassword(mail, cl);
 promise.catch(e => console.log(e.message));
-
+let prub = user1.value;
+  if (validateEmail(prub)){
+    mailCorrect.innerHTML = ('Tu correo es correcto');
+  }
+  else{
+    mailCorrect.innerHTML =  ('Ingrese un correo valido por favor');
+  }
+  // validateEmail(user1 );
 });
+// INICIAR SESION.
+btn.addEventListener('click', e =>{
 
-btnUser.addEventListener('click', e =>{
   const mail1 = usuario.value;
   
   const cl1 = clave.value;
   
   const auth = firebase.auth();
- 
 const promise = auth.signInWithEmailAndPassword(mail1, cl1);
-promise.catch(e => console.log(e.message));
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // loadSesion = () =>{
-  //   document.getElementById('cuenta1').addEventListener('click',(event)=>{
-  //     const usuario1 = document.getElementById('user1');
-  //     const clave1 = document.getElementById('password1');
-  //     const btn1 = document.getElementById('cuenta1');
-  //     let  userone = usuario1.value;
-  //     console.log(userone);
-  //     let cla1 = clave1.value;
-  //     let bt1 = btn1.value;
-
-  //   });
+promise.catch(error => {
+  let prub = user1.value;
+  if (validateEmail(prub)){
+    mailCorrect.innerHTML = ('Tu correo es correcto');
+  }
+  else{
+    mailCorrect.innerHTML =  ('Ingrese un correo valido por favor');
+  } 
   
+} );
 
 
-  // }
-  // loadSesion();
-
-  // Creacion de usuario view1
-  // createUser = () =>{
-  //   document.getElementById('account').addEventListener('click',(event)=>{
-  //     const email = document.getElementById('email');
-  //     const clave = document.getElementById('password');
-  //     const btn = document.getElementById('account');
-  //     const auth = firebase.auth();
-  //     const promise = auth.createUserWithEmailAndPassword(email,clave);
-  //     promise.catch(e => console.log(e.message));
-  //     promise.push('createUserWithEmailAndPassword'); 
-  //     let  mail = email.value;
-  //     console.log(mail);
-  //     let cla = clave.value;
-  //     console.log(cla);
-  //     let bt = btn.value;
-   
-        
-       
-  //     });
-     
-     
-      
-    
-
-  //   };
-  //  createUser();
-
-
-
-
-
-
-
-
-
-
-
-
-//   // const usuario = document.getElementById('user');
-//   // const clave = document.getElementById('password');
-//   // const btn = document.getElementById('cuenta');
-//   // const usuario1 = document.getElementById('user1');
-//   // const clave1 = document.getElementById('password1');
-//   // const btn1 = document.getElementById('cuenta1');
-  
-//   // console.log(usuario);
-//   createUser = ()=>{
-// document.getElementById('account1').addEventListener('click', e =>{
-// const email = usuario.value;
-// // console.log(email);
-// const cl = clave.value;
-// // console.log(cl);
-// const auth = firebase.auth();
-
-// const promise = auth.createUserWithEmailAndPassword(email, cl);
-// promise.catch(e => console.log(e.message));
-// }
-// // function pintar
-// // btn.addEventListener('click', e =>{
-// //   const email1 = usuario.value;
-// //   // console.log(email);
-// //   const cl1 = clave.value;
-// //   // console.log(cl);
-// //   const auth = firebase.auth(); 
-// //   const promise = auth.createUserWithEmailAndPassword(email, cl);
-//   promise.catch(e => console.log(e.message));
-
-
-  
-
-
-  
+ });
+ 
