b9dbf1050d2af5e8a053d64aeeb57ed452d6cfcf
Josce Jaimes <nanayhachi94@gmail>
2018-07-25 11:12:42 -0500

    agrega funcionalidad de firebase para el muro

--- src/js/app.js
+++ src/js/app.js
@@ -7,24 +7,82 @@
     storageBucket: "bucket.appspot.com"
   };
   firebase.initializeApp(config);
+  //para ingresar a guardar la data 
+  var storage = firebase.storage();
+
   // Get a reference to the database service
   var database = firebase.database();
  
+
+
  
-  const btnsave= document.getElementById('save')
+  const btnsave= document.getElementById('public')
   const btnedit = document.getElementById('edit')
   //input para comentar
   const validComenter = document.getElementById('comenters')
   const comentPage = document.getElementById('comment')
- 
- 
+
   btnsave.addEventListener('click', e =>{
       let comentario = validComenter.value;
        comment.innerHTML = comentario
+      //  object = firebase.database().ref().child('user1')
+       writeUserData();
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+//   const btnsave= document.getElementById('public')
+//   const btnedit = document.getElementById('edit')
+//   //input para comentar
+//   const validComenter = document.getElementById('comenters')
+//   const comentPage = document.getElementById('comment')
+//   const perfil = document.getElementById('user1')
  
  
- //   edit.addEventListener('click', e =>{
+
+// const object = firebase.database().ref().child('user1')
+
+//   btnsave.addEventListener('click', e =>{
+//       let comentario = validComenter.value;
+//        comment.innerHTML = comentario
+//       //  object = firebase.database().ref().child('user1')
+//        writeUserData();
+//   });
+ 
+//   writeUserData = (user1, name, email, imageUrl)=> {
+//     firebase.database().ref().child(user1)({
+//       username: name,
+//       email: email,
+//       profile_picture : imageUrl
+      
+//     });
+//     console.log(writeUserData);
+//   }
+  
+//  //   edit.addEventListener('click', e =>{
  
  
- //   })
+//  //   })
