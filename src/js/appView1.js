83242ad5a0e7463eaad30b645a0a7e051d452e71
unknown <nan.si_1305@hotmail.com>
2018-07-31 23:03:33 -0500

    arreglando lo de like y dislike porque no funcionaba

--- src/js/appView1.js
+++ src/js/appView1.js
@@ -1,5 +1,5 @@
 const taskInput = document.getElementById('new-task');
-const addButton = document.getElementsByTagName('Button')[0];
+const addButton = document.getElementsByTagName('Button')[1];
 const inCompletedTaskList = document.getElementById('incomplete-tasks');
 const completedTaskList = document.getElementById('completed-tasks');
 
@@ -11,35 +11,55 @@ const init = () => {
   getTaskOfFirebase();
 }
 
-const createNewTaskElement = (taskString) => {
+const addTask = (key, taskCollection) => {
+  const listItem = createNewTaskElement(key,taskCollection);
+  listItem.setAttribute('data-keytask', key);
+    completedTaskList.appendChild(listItem);
+
+
+  bindTaskEvents(listItem, taskCompleted)
+}
+const createNewTaskElement = (key,taskString) => {
+  
   const listItem = document.createElement('p'); //creamos un nuevo elemento cada que se ingrese un nuevo comentario
   const label = document.createElement('label');
   const editButton = document.createElement('button');
   const deleteButton = document.createElement('button');
 
+  const totalLikes = document.createElement('div');
+
+  //const likeButton = document.createElement('button');
+  totalLikes.innerHTML = "Total de likes "+taskString.like;
 
   editButton.innerHTML = 'Edit &#9998;';
   editButton.className = 'edit';
   deleteButton.innerHTML = 'Delete &#x1F5D1;';
   deleteButton.className = 'delete';
 
-  label.innerHTML = taskString;
+  label.innerHTML = taskString.contenidoTask;
 
+  const btn = document.createElement('input');
+  btn.setAttribute('type', 'button'); // input element of type button
+  btn.setAttribute('value', 'Like');
+  let user = firebase.auth().currentUser;
+  console.log(taskString);
+  let inArray = taskString.likeUser.indexOf(user.uid);
+
+  if(inArray>0){
+    btn.setAttribute('class', 'Like');
+    btn.onclick = function() { deleteLike(key); };
+  }else{
+    btn.onclick = function() { addLike(key); };
+  }
   listItem.appendChild(label);
-  listItem.appendChild(editButton);
-  listItem.appendChild(deleteButton);
+  if(user.uid===taskString.userId){
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+  }
+  listItem.appendChild(btn);
+  listItem.appendChild(totalLikes);
 
   return listItem;
-}
-
-
-const addTask = (key, taskCollection) => {
-  const listItem = createNewTaskElement(taskCollection.contenidoTask);
-  listItem.setAttribute('data-keytask', key);
-    completedTaskList.appendChild(listItem);
-
-
-  bindTaskEvents(listItem, taskCompleted)
 }
 
 const taskCompleted = () => {
@@ -69,10 +89,10 @@ const taskCompleted = () => {
 const bindTaskEvents = (taskListItem, checkboxEventHandle) => {
   const editButton = taskListItem.querySelector('button.edit');
   const deleteButton = taskListItem.querySelector('button.delete');
-
-  editButton.addEventListener('click', editTask);
-
-  deleteButton.addEventListener('click', deleteTask);
+  if(editButton){
+    editButton.addEventListener('click', editTask);
+    deleteButton.addEventListener('click', deleteTask);
+  }
 }
 
 const editTask = () => {
@@ -116,6 +136,7 @@ const deleteTask = () => {
 
 const getTaskOfFirebase = () => {
   refTask.on('value', (snapshot) => {
+    console.log(snapshot.val());
     inCompletedTaskList.innerHTML = '';
     completedTaskList.innerHTML = '';
     const data = snapshot.val()
@@ -126,17 +147,21 @@ const getTaskOfFirebase = () => {
 }
 
 const sendTaskFirebase = () => {
+  var user = firebase.auth().currentUser;
+  ///console.log(user.uid);
   if(taskInput.value.trim() != ''){ //funcion nativa de JS que elimina los espacios de una cadena
+    let arrayUser = {0:1};
     refTask.push({
-      contenidoTask : taskInput.value
-      //status : 'incomplete'
+      contenidoTask : taskInput.value,
+      userId : user.uid,
+      like:0,
+      likeUser :arrayUser
     });
     taskInput.value = '';
   }else{
     alert('No puedes realizar una publicación en blanco.')
   }
 }
-
 
 const btnCloseSesion = document.getElementById('close')
   btnCloseSesion.addEventListener('click', e =>{
@@ -152,4 +177,40 @@ firebase.auth().onAuthStateChanged(function (user) {
   document.getElementById('user-name').innerHTML =user.displayName;
   //alert(user.displayName);
 });
+
+const addLike = (idComentario) => {
+  var user = firebase.auth().currentUser;
+  const dataComentario = refTask.child(idComentario);
+  dataComentario.once('value', (snapshot) => {
+    const data = snapshot.val();
+    data.likeUser.push(user.uid);
+    dataComentario.update({
+      like: data.like+1,
+      likeUser:data.likeUser
+    })
+  });
+}
+const deleteLike = (idComentario) => {
+  var user = firebase.auth().currentUser;
+  const dataComentario = refTask.child(idComentario);
+  dataComentario.once('value', (snapshot) => {
+    const data = snapshot.val();
+    //data.likeUser.push(user.uid);
+    for (var i = 0; i < data.likeUser.length; i++)
+    if (data.likeUser[i] === user.uid) { 
+      data.likeUser.splice(i, 1);
+        break;
+    }
+    console.log(data.likeUser);
+    const like = data.like-1
+    if(like<0){
+      like = 0;
+    }
+    dataComentario.update({
+      like:like,
+      likeUser:data.likeUser
+    })
+  });
+}
+
 window.onload = init
